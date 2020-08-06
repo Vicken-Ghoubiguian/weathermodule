29ab813cc6f3ad243ce7d213713dbdba40cbbd4e
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-08-06 10:12:01 +0200

    Developping the Coordinates type's initializer

--- src/weatherClasses/coordinates.go
+++ src/weatherClasses/coordinates.go
@@ -4,6 +4,12 @@ package weatherClasses
 type Coordinates struct {
 
 	//
-	longitude string
-	latitude string
+	longitude float64
+	latitude float64
+}
+
+//
+func InitializeCoordinates(wishedongitude float64, wishedLatitude float64) *Coordinates {
+
+	return &Coordinates{longitude: wishedongitude, latitude: wishedLatitude}
 }
