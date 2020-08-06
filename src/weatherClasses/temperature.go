a98881c1a150973ed535678c4a5747daa632cf75
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-08-06 12:43:26 +0200

    Writting comments for the getters

--- src/weatherClasses/temperature.go
+++ src/weatherClasses/temperature.go
@@ -26,25 +26,25 @@ type Temperature struct {
 	temperatureScaleSymbol string
 }
 
-//
+// Defining the Temperature initializer
 func InitializeTemperature(value float64) *Temperature {
 
 	return &Temperature{temperatureValue: value, currentTemperatureScale: Kelvin, temperatureScaleSymbol: " K"}
 }
 
-//
+// 'temperatureValue' attribute getter
 func (temp *Temperature) GetTemperatureValue() float64 {
 
 	return temp.temperatureValue
 }
 
-//
+// 'currentTemperatureScale' attribute getter
 func (temp *Temperature) GetCurrentTemperatureScale() TemperatureScale {
 
 	return temp.currentTemperatureScale
 }
 
-//
+// 'temperatureScaleSymbol' attribute getter
 func (temp *Temperature) GetTemperatureScaleSymbol() string {
 
 	return temp.temperatureScaleSymbol
