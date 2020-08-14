d640cda5e7eb7e9df5a3644460b1fb62831e3575
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-08-14 12:05:32 +0200

    Defining all Weather type's getters

--- src/weatherClasses/weather.go
+++ src/weatherClasses/weather.go
@@ -14,3 +14,27 @@ func InitializeWeather(idValue int, mainValue string, descriptionValue string, i
 
 	return &Weather{id: idValue, main: mainValue, description: descriptionValue, iconUrl: "https://openweathermap.org/img/wn/" + icon + ".png"}
 }
+
+//
+func (currentWeather *Weather) GetId() int {
+
+	return currentWeather.id
+}
+
+//
+func (currentWeather *Weather) GetMain() string {
+
+	return currentWeather.main
+}
+
+//
+func (currentWeather *Weather) GetDescription() string {
+
+	return currentWeather.description
+}
+
+//
+func (currentWeather *Weather) GetIconUrl() string {
+
+	return currentWeather.iconUrl
+}
