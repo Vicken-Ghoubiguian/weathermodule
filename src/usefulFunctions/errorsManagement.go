fe8cfe4d7d9a8c201f1e79724015712bffb3c3b6
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-09-13 17:15:28 +0200

    Definition of another package with functions

---
+++ src/usefulFunctions/errorsManagement.go
@@ -0,0 +1,28 @@
+package usefulFunctions
+
+// Import all necessary packages
+import (
+	"fmt"
+	"os"
+)
+
+// Function which display HTTP request error's code and message when the first occurs, with color and reset strings
+func owmErrorHandlerFunction(codeError string, color string, errorMessage string, reset string) {
+
+        fmt.Println(color + "Occured error (" + codeError + "): " + errorMessage + reset)
+
+        fmt.Println("\n")
+
+        os.Exit(1)
+}
+
+// Function which display other errors when they occurs, with color and reset strings
+func otherErrorHandlerFunction(err error, color string, reset string) {
+
+        if err != nil {
+
+                fmt.Println(color + err.Error() + reset)
+
+                os.Exit(1)
+        }
+}
