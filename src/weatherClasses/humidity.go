081a1dc0a667897e3085c1ce1b3af59280547637
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-09-27 09:21:56 +0200

    Definition of Humidity type

---
+++ src/weatherClasses/humidity.go
@@ -0,0 +1,27 @@
+package weatherClasses
+
+//
+type Humidity struct {
+
+	value int64
+	unitScale string
+}
+
+//
+func (currentHumidity *Humidity) InitializeHumidity(humidityValue int64) {
+
+	currentHumidity.value = humidityValue
+	currentHumidity.unitScale = "%"
+}
+
+//
+func (currentHumidity *Humidity) GetHumidityValue() int64 {
+
+	return currentHumidity.value
+}
+
+//
+func (currentHumidity *Humidity) GetHumidityUnitScale() string {
+
+	return currentHumidity.unitScale
+}
