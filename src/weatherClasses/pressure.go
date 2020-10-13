923acb46fcd279120cebf377c30b45c530b5b62b
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2020-10-13 20:08:08 +0200

    Writting comments

--- src/weatherClasses/pressure.go
+++ src/weatherClasses/pressure.go
@@ -13,7 +13,7 @@ const (
 	Torr
 )
 
-//
+// pressure constructor with all needed parameters
 func (pressureUnitScale PressureScale) String() string {
 
 	return [...]string{"hectoPascal", "pascal", "bar", "atmosphere", "torr"}[pressureUnitScale]
@@ -35,49 +35,49 @@ func (currentPressure *Pressure) InitializePressure(pressureValue float64) {
 	currentPressure.symbolUnit = "hPa"
 }
 
-//
+// set pressure to HectoPascal (hPa)
 func (currentPressure *Pressure) setPressureAsHectoPascal() {
 
 }
 
-//
+// set pressure to Pascal (Pa)
 func (currentPressure *Pressure) setPressureAsPascal() {
 
 }
 
-//
+// set pressure to Bar
 func (currentPressure *Pressure) setPressureAsBar() {
 
 }
 
-//
+// set pressure to Atmosphere
 func (currentPressure *Pressure) setPressureAsAtmosphere() {
 
 }
 
-//
+// set pressure to Torr
 func (currentPressure *Pressure) setPressureAsTorr() {
 
 }
 
-//
+// set pressure to Pounds per square inch (psi)
 func (currentPressure *Pressure) setPressureAsPoundsPerSquareInch() {
 
 }
 
-//
+// 'value' attribute getter
 func (currentPressure *Pressure) getPressureValue() float64 {
 
 	return currentPressure.value
 }
 
-//
+// 'scaleUnit' attribute getter
 func (currentPressure *Pressure) getPressureScale() PressureScale {
 
 	return currentPressure.scaleUnit
 }
 
-//
+// 'symbolUnit' attribute getter
 func (currentPressure *Pressure) getPressureSymbolUnit() string {
 
 	return currentPressure.symbolUnit
