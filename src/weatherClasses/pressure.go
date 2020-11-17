cf02b45efbe280a59f9696e37cf1eb1ea97500e4
Eric Ghoubiguian <ericghoubiguian@live.fr>
2020-11-17 11:13:25 +0100

    Writing the pressure conversion functions body

--- src/weatherClasses/pressure.go
+++ src/weatherClasses/pressure.go
@@ -1,6 +1,11 @@
 package weatherClasses
 
 //
+import (
+	"fmt"
+)
+
+//
 type PressureScale int
 
 //
@@ -39,31 +44,271 @@ func (currentPressure *Pressure) InitializePressure(pressureValue float64) {
 // set pressure to HectoPascal (hPa)
 func (currentPressure *Pressure) setPressureAsHectoPascal() {
 
+	if currentPressure.scaleUnit == Pascal {
+
+		currentPressure.scaleUnit = HectoPascal
+		currentPressure.symbolUnit = " hPa"
+
+		fmt.Print(Green() + "Pressure converted in HectoPascal (hPa) successfully." + Reset() + "\n")
+
+	} else if currentPressure.scaleUnit == Bar {
+
+		currentPressure.scaleUnit = HectoPascal
+		currentPressure.symbolUnit = " hPa"
+
+		fmt.Print(Green() + "Pressure converted in HectoPascal (hPa) successfully." + Reset() + "\n")
+
+	} else if currentPressure.scaleUnit == Atmosphere {
+
+		currentPressure.scaleUnit = HectoPascal
+		currentPressure.symbolUnit = " hPa"
+
+		fmt.Print(Green() + "Pressure converted in HectoPascal (hPa) successfully." + Reset() + "\n")
+
+	} else if currentPressure.scaleUnit == Torr {
+
+		currentPressure.scaleUnit = HectoPascal
+		currentPressure.symbolUnit = " hPa"
+
+		fmt.Print(Green() + "Pressure converted in HectoPascal (hPa) successfully." + Reset() + "\n")
+
+	} else if currentPressure.scaleUnit == PoundsPerSquareInch {
+
+		currentPressure.scaleUnit = HectoPascal
+		currentPressure.symbolUnit = " hPa"
+
+		fmt.Print(Green() + "Pressure converted in HectoPascal (hPa) successfully." + Reset() + "\n")
+
+	} else {
+
+		fmt.Print(Red() + "Pressure already in HectoPascal (hPa)." + Reset() + "\n")
+
+	}
 }
 
 // set pressure to Pascal (Pa)
 func (currentPressure *Pressure) setPressureAsPascal() {
 
+	if currentPressure.scaleUnit == HectoPascal {
+
+                currentPressure.scaleUnit = Pascal
+                currentPressure.symbolUnit = " Pa"
+
+                fmt.Print(Green() + "Pressure converted in Pascal (Pa) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Bar {
+
+                currentPressure.scaleUnit = Pascal
+                currentPressure.symbolUnit = " Pa"
+
+                fmt.Print(Green() + "Pressure converted in Pascal (Pa) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Atmosphere {
+
+                currentPressure.scaleUnit = Pascal
+                currentPressure.symbolUnit = " Pa"
+
+                fmt.Print(Green() + "Pressure converted in Pascal (Pa) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Torr {
+
+                currentPressure.scaleUnit = Pascal
+                currentPressure.symbolUnit = " Pa"
+
+                fmt.Print(Green() + "Pressure converted in Pascal (Pa) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == PoundsPerSquareInch {
+
+                currentPressure.scaleUnit = Pascal
+                currentPressure.symbolUnit = " Pa"
+
+                fmt.Print(Green() + "Pressure converted in Pascal (Pa) successfully." + Reset() + "\n")
+
+        } else {
+
+                fmt.Print(Red() + "Pressure already in Pascal (Pa)." + Reset() + "\n")
+
+        }
 }
 
 // set pressure to Bar
 func (currentPressure *Pressure) setPressureAsBar() {
 
+	if currentPressure.scaleUnit == HectoPascal {
+
+                currentPressure.scaleUnit = Bar
+                currentPressure.symbolUnit = " bar"
+
+                fmt.Print(Green() + "Pressure converted in Bar (bar) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Pascal {
+
+                currentPressure.scaleUnit = Bar
+                currentPressure.symbolUnit = " bar"
+
+                fmt.Print(Green() + "Pressure converted in Bar (bar) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Atmosphere {
+
+                currentPressure.scaleUnit = Bar
+                currentPressure.symbolUnit = " bar"
+
+                fmt.Print(Green() + "Pressure converted in Bar (bar) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Torr {
+
+                currentPressure.scaleUnit = Bar
+                currentPressure.symbolUnit = " bar"
+
+                fmt.Print(Green() + "Pressure converted in Bar (bar) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == PoundsPerSquareInch {
+
+                currentPressure.scaleUnit = Bar
+                currentPressure.symbolUnit = " bar"
+
+                fmt.Print(Green() + "Pressure converted in Bar (bar) successfully." + Reset() + "\n")
+
+        } else {
+
+                fmt.Print(Red() + "Pressure already in Bar (bar)." + Reset() + "\n")
+
+        }
 }
 
 // set pressure to Atmosphere
 func (currentPressure *Pressure) setPressureAsAtmosphere() {
 
+	if currentPressure.scaleUnit == HectoPascal {
+
+                currentPressure.scaleUnit = Atmosphere
+                currentPressure.symbolUnit = " atm"
+
+                fmt.Print(Green() + "Pressure converted in Atmosphere (atm) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Pascal {
+
+                currentPressure.scaleUnit = Atmosphere
+                currentPressure.symbolUnit = " atm"
+
+                fmt.Print(Green() + "Pressure converted in Atmosphere (atm) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Bar {
+
+                currentPressure.scaleUnit = Atmosphere
+                currentPressure.symbolUnit = " atm"
+
+                fmt.Print(Green() + "Pressure converted in Atmosphere (atm) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Torr {
+
+                currentPressure.scaleUnit = Atmosphere
+                currentPressure.symbolUnit = " atm"
+
+                fmt.Print(Green() + "Pressure converted in Atmosphere (atm) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == PoundsPerSquareInch {
+
+                currentPressure.scaleUnit = Atmosphere
+                currentPressure.symbolUnit = " atm"
+
+                fmt.Print(Green() + "Pressure converted in Atmosphere (atm) successfully." + Reset() + "\n")
+
+        } else {
+
+                fmt.Print(Red() + "Pressure already in Atmosphere (atm)." + Reset() + "\n")
+
+        }
 }
 
 // set pressure to Torr
 func (currentPressure *Pressure) setPressureAsTorr() {
 
+	if currentPressure.scaleUnit == HectoPascal {
+
+                currentPressure.scaleUnit = Torr
+                currentPressure.symbolUnit = " torr"
+
+                fmt.Print(Green() + "Pressure converted in Torr (torr) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Bar {
+
+                currentPressure.scaleUnit = Torr
+                currentPressure.symbolUnit = " torr"
+
+                fmt.Print(Green() + "Pressure converted in Torr (torr) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Atmosphere {
+
+                currentPressure.scaleUnit = Torr
+                currentPressure.symbolUnit = " torr"
+
+                fmt.Print(Green() + "Pressure converted in Torr (torr) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Pascal {
+
+                currentPressure.scaleUnit = Torr
+                currentPressure.symbolUnit = " torr"
+
+                fmt.Print(Green() + "Pressure converted in Torr (torr) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == PoundsPerSquareInch {
+
+                currentPressure.scaleUnit = Torr
+                currentPressure.symbolUnit = " torr"
+
+                fmt.Print(Green() + "Pressure converted in Torr (torr) successfully." + Reset() + "\n")
+
+        } else {
+
+                fmt.Print(Red() + "Pressure already in Torr (torr)." + Reset() + "\n")
+
+        }
 }
 
 // set pressure to Pounds per square inch (psi)
 func (currentPressure *Pressure) setPressureAsPoundsPerSquareInch() {
 
+	if currentPressure.scaleUnit == HectoPascal {
+
+                currentPressure.scaleUnit = PoundsPerSquareInch
+                currentPressure.symbolUnit = " psi"
+
+                fmt.Print(Green() + "Pressure converted in Pounds per square inch (psi) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Bar {
+
+                currentPressure.scaleUnit = PoundsPerSquareInch
+                currentPressure.symbolUnit = " psi"
+
+                fmt.Print(Green() + "Pressure converted in Pounds per square inch (psi) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Atmosphere {
+
+                currentPressure.scaleUnit = PoundsPerSquareInch
+                currentPressure.symbolUnit = " psi"
+
+                fmt.Print(Green() + "Pressure converted in Pounds per square inch (psi) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Pascal {
+
+                currentPressure.scaleUnit = PoundsPerSquareInch
+                currentPressure.symbolUnit = " psi"
+
+                fmt.Print(Green() + "Pressure converted in Pounds per square inch (psi) successfully." + Reset() + "\n")
+
+        } else if currentPressure.scaleUnit == Torr {
+
+                currentPressure.scaleUnit = PoundsPerSquareInch
+                currentPressure.symbolUnit = " psi"
+
+                fmt.Print(Green() + "Pressure converted in Pounds per square inch (psi) successfully." + Reset() + "\n")
+
+        } else {
+
+                fmt.Print(Red() + "Pressure already in Pounds per square inch (psi)." + Reset() + "\n")
+
+        }
 }
 
 // 'value' attribute getter
